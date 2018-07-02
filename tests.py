fe495140cec887dbff534db9e868e01a7e0ba50a
Jonny Evans <jonny.holomorphic@gmail.com>
2018-07-02 16:08:17 +0100

    Modified tests.py to make it easier to use just by changing some variables.

--- tests.py
+++ tests.py
@@ -5,16 +5,21 @@ from epr import *
 from operator import itemgetter
 from itertools import groupby
 
-lens_spaces=epr_by_delta(30,30,10)
+delta=2
+P1=30
+P2=30
+C=10
+
+lens_spaces=epr_by_delta(P1,P2,C)
 monod=[]
 
 f = lambda a,b: BOLF.simplify(BOLF.find(a,b,2))
 
-for lens in lens_spaces[2]:
+for lens in lens_spaces[delta]:
     a,b=lens
     monod.append({'lens':lens,'factor':f(a,b)})
 monod.sort(key=itemgetter('factor'))
 for factor,items in groupby(monod,key=itemgetter('factor')):
     print(factor)
     for i in items:
-        print('    ',i['lens'])
+        print('    L(',i['lens'],')')
